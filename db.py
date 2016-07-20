02338fe63d87a1f5b3feb3adf7fb5ecc9484bc27
raylu <lurayl@gmail.com>
2016-07-20 00:04:07 -0700

    toggle draft, view post

--- db.py
+++ db.py
@@ -12,10 +12,6 @@ db = plyvel.DB(db_path, create_if_missing=True)
 users_db = db.prefixed_db(b'user-')
 posts_db = db.prefixed_db(b'post-')
 
-class PostFlag(IntEnum):
-	deleted = 0x00000001
-	draft = 0x00000002
-
 def has_users():
 	with users_db.iterator() as it:
 		try:
@@ -46,15 +42,31 @@ def create_post(username, title, body):
 		batch.put(post_id, post_data)
 		batch.put(b'count', post_id)
 
+def toggle_post_flag(post_id, flag, status):
+	post_id = _pad_num(post_id)
+	post = posts_db.get(post_id)
+	flags = struct.unpack('I', post[:4])[0]
+	if status:
+		flags |= flag
+	else:
+		flags &= ~flag
+	post_data = struct.pack('I', flags) + post[4:]
+	posts_db.put(post_id, post_data)
+
+def get_post(post_id, allowed_flags):
+	post = posts_db.get(_pad_num(post_id))
+	if post is not None:
+		return _parse_post(post, allowed_flags)
+
 def iter_posts(allowed_flags):
 	post_count = posts_db.get(b'count', b'000000')
 	with posts_db.iterator(stop=post_count, include_stop=True) as it:
 		for post_id, post in it:
-			flags = struct.unpack('I', post[:4])[0]
-			if flags & allowed_flags != flags:
+			parsed = _parse_post(post, allowed_flags)
+			if parsed is None:
 				continue
-			username, title, body = post[4:].split(b'\0')
-			yield flags, post_id, username.decode('utf-8'), title.decode('utf-8'), body.decode('utf-8')
+			post_id = int(post_id)
+			yield (post_id,) + parsed
 
 def _pad_num(n):
 	''' 5 -> b'000005' '''
@@ -62,6 +74,27 @@ def _pad_num(n):
 	buf = bytearray(b'000000')
 	buf[-len(encoded):] = encoded
 	return bytes(buf)
+
+def _parse_post(post, allowed_flags):
+	flags = struct.unpack('I', post[:4])[0]
+	if flags & allowed_flags != flags:
+		return None
+	pf = PostFlags(flags)
+	username, title, body = post[4:].split(b'\0')
+	username = username.decode('utf-8')
+	title = title.decode('utf-8')
+	body = body.decode('utf-8')
+	return pf, username, title, body
+
+class PostFlag(IntEnum):
+	deleted = 0x00000001
+	draft = 0x00000002
+
+class PostFlags:
+	def __init__(self, flags):
+		for name, bit in PostFlag.__members__.items():
+			value = flags & bit == bit
+			setattr(self, name, value)
 
 def close():
 	db.close()
